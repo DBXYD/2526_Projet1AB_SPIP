9e52630a9bc7afda2e4a0209145ca3fc33ab2d45
ANDRIEU2028 <lucas.andrieu@ensea.fr>
2026-02-19 07:50:32 +0100

    mise à jour de la schématique du PCB

--- Firmware/PAMi/Core/Inc/main.h
+++ Firmware/PAMi/Core/Inc/main.h
@@ -49,8 +49,6 @@ extern "C" {
 
 /* USER CODE END EM */
 
-void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
-
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -59,22 +57,54 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define BIN1_Pin GPIO_PIN_2
-#define BIN1_GPIO_Port GPIOA
-#define BIN2_Pin GPIO_PIN_3
-#define BIN2_GPIO_Port GPIOA
-#define X1_Pin GPIO_PIN_4
-#define X1_GPIO_Port GPIOA
-#define X2_Pin GPIO_PIN_5
-#define X2_GPIO_Port GPIOA
-#define X3_Pin GPIO_PIN_6
-#define X3_GPIO_Port GPIOA
-#define X4_Pin GPIO_PIN_7
-#define X4_GPIO_Port GPIOA
-#define AIN1_Pin GPIO_PIN_11
-#define AIN1_GPIO_Port GPIOA
-#define AIN2_Pin GPIO_PIN_12
-#define AIN2_GPIO_Port GPIOA
+#define US_TRIG_Pin GPIO_PIN_0
+#define US_TRIG_GPIO_Port GPIOA
+#define US_ECHO_INT_Pin GPIO_PIN_1
+#define US_ECHO_INT_GPIO_Port GPIOA
+#define UART_STLINK_TX_Pin GPIO_PIN_2
+#define UART_STLINK_TX_GPIO_Port GPIOA
+#define UART_STLINK_RX_Pin GPIO_PIN_3
+#define UART_STLINK_RX_GPIO_Port GPIOA
+#define X1_INT_Pin GPIO_PIN_4
+#define X1_INT_GPIO_Port GPIOA
+#define X2_INT_Pin GPIO_PIN_5
+#define X2_INT_GPIO_Port GPIOA
+#define X3_INT_Pin GPIO_PIN_6
+#define X3_INT_GPIO_Port GPIOA
+#define X4_INT_Pin GPIO_PIN_7
+#define X4_INT_GPIO_Port GPIOA
+#define UART_XL320_Pin GPIO_PIN_4
+#define UART_XL320_GPIO_Port GPIOC
+#define MotorFault_Pin GPIO_PIN_10
+#define MotorFault_GPIO_Port GPIOB
+#define USR_BTN_3_Pin GPIO_PIN_12
+#define USR_BTN_3_GPIO_Port GPIOB
+#define USR_BTN_2_Pin GPIO_PIN_13
+#define USR_BTN_2_GPIO_Port GPIOB
+#define USR_BTN_1_Pin GPIO_PIN_14
+#define USR_BTN_1_GPIO_Port GPIOB
+#define USR_LED_3_Pin GPIO_PIN_15
+#define USR_LED_3_GPIO_Port GPIOB
+#define USR_LED_2_Pin GPIO_PIN_6
+#define USR_LED_2_GPIO_Port GPIOC
+#define USR_LED_1_Pin GPIO_PIN_8
+#define USR_LED_1_GPIO_Port GPIOA
+#define MotorB_PWM_Forward_Pin GPIO_PIN_9
+#define MotorB_PWM_Forward_GPIO_Port GPIOA
+#define MotorB_PWM_Reverse_Pin GPIO_PIN_10
+#define MotorB_PWM_Reverse_GPIO_Port GPIOA
+#define MotorA_PWM_Forward_Pin GPIO_PIN_15
+#define MotorA_PWM_Forward_GPIO_Port GPIOA
+#define MotorA_PWM_Reverse_Pin GPIO_PIN_3
+#define MotorA_PWM_Reverse_GPIO_Port GPIOB
+#define MotorA_ENC_A_Pin GPIO_PIN_4
+#define MotorA_ENC_A_GPIO_Port GPIOB
+#define MotorA_ENC_B_Pin GPIO_PIN_5
+#define MotorA_ENC_B_GPIO_Port GPIOB
+#define MotorB_ENC_A_Pin GPIO_PIN_6
+#define MotorB_ENC_A_GPIO_Port GPIOB
+#define MotorB_ENC_B_Pin GPIO_PIN_7
+#define MotorB_ENC_B_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
